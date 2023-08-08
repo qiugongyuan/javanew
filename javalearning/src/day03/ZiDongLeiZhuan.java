14f176d293899d9b9468da8b8cf1794de28a099b
qiugongyuan <360960443@qq.com>
2023-08-08 09:03:46 +0800

    变量

---
+++ javalearning/src/day03/ZiDongLeiZhuan.java
@@ -0,0 +1,13 @@
+package day03;
+
+public class ZiDongLeiZhuan {
+	public static void main(String[] args) {
+		char c1='a';
+		int i1=c1;
+		System.out.println("char类型自动转换成int后的值是："+i1);
+		char c2='a';
+		int i2=c2+1;
+		System.out.println("char类型和int类型相加后计算的值："+i2);
+	}
+
+}
